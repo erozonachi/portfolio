a74c0c750112cc23bbd552e1a618086e9ae3ada2
James Eneh <erozonachi.james@gmail.com>
2019-10-14 23:30:02 +0100

    Create and export bg image styled component

---
+++ src/components/styled-components/background-image.js
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+const BGImage = styled.img`
+  position: absolute;
+  left: 0;
+  top: 0;
+  max-width: 1440px;
+  z-index: -1;
+`
+
+export default BGImage
---
+++ src/components/styled-components/index.js
@@ -0,0 +1,3 @@
+import BGImage from "./background-image"
+
+export { BGImage }
